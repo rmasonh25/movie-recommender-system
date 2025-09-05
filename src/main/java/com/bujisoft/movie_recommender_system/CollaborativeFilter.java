554bef0e9a4494b631164a2b765e410e12705e3e
rmasonh25 <rmasonh25@gmail.com>
2025-09-05 11:43:02 -0400

    Change content filter to collaborative filter

---
+++ src/main/java/com/bujisoft/movie_recommender_system/CollaborativeFilter.java
@@ -0,0 +1,19 @@
+/*
+ *
+ *
+ * Copyright (c) 2025 Bujisoft
+ * Author: Robert Hendricks
+ * File: CollaborativeFilter.java
+ * Created on: 2025-09-05 11:38
+ * Last modified: 2025-09-05 11:38
+ *
+ */
+
+package com.bujisoft.movie_recommender_system;
+
+public class CollaborativeFilter {
+    public String[] getRecommendations(String movie) {
+        // logic of collaborative filter
+        return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+    }
+}
--- src/main/java/com/bujisoft/movie_recommender_system/RecommenderImplementation.java
+++ src/main/java/com/bujisoft/movie_recommender_system/RecommenderImplementation.java
@@ -4,8 +4,8 @@
  * Copyright (c) 2025 Bujisoft
  * Author: Robert Hendricks
  * File: RecommenderImplementation.java
- * Created on: 2025-09-05 02:56
- * Last modified: 2025-09-05 02:56
+ * Created on: 2025-09-05 11:40
+ * Last modified: 2025-09-05 11:40
  *
  */
 
@@ -14,8 +14,10 @@ package com.bujisoft.movie_recommender_system;
 public class RecommenderImplementation {
     public String[] recommendMovies(String movie) {
         // use content based filter to find movies
-        ContentBasedFilter filter = new ContentBasedFilter();
-        String[] results = filter.getRecommendations(movie);
+        // change filter to collaborative filter to find movies
+        /// ContentBasedFilter filter = new ContentBasedFilter();
+        CollaborativeFilter filter = new CollaborativeFilter();
+        String[] results = filter.getRecommendations("Finding Dory");
         //return the results
         return results;
        // return new String[] {"M1", "M2", "M3"};
