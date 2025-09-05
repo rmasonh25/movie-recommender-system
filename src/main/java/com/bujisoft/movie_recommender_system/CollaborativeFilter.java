1e370d0c8e81a88c5f068b4d6f7f8248a5dec848
rmasonh25 <rmasonh25@gmail.com>
2025-09-05 12:30:11 -0400

    adding more than 2 beans -- making collaborative filter as primary

--- src/main/java/com/bujisoft/movie_recommender_system/CollaborativeFilter.java
+++ src/main/java/com/bujisoft/movie_recommender_system/CollaborativeFilter.java
@@ -4,17 +4,19 @@
  * Copyright (c) 2025 Bujisoft
  * Author: Robert Hendricks
  * File: CollaborativeFilter.java
- * Created on: 2025-09-05 12:24
- * Last modified: 2025-09-05 12:24
+ * Created on: 2025-09-05 12:29
+ * Last modified: 2025-09-05 12:29
  *
  */
 
 package com.bujisoft.movie_recommender_system;
 
 
-//import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
+@Primary
 public class CollaborativeFilter implements Filter{
     @Override
     public String[] getRecommendations(String movie) {
